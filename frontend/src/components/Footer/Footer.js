afe94d9724d61b3736a03254a2af379b98132ee0
asleulv <101048079+asleulv@users.noreply.github.com>
2024-08-06 13:55:10 +0200

    Styling for mobile. Smaller text on FrontPage

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -6,14 +6,19 @@ const Footer = () => {
 
   // Define the inline styles
   const footerStyle = {
-    backgroundColor: 'hotpink', // Background color
+    backgroundColor: '#BE2274', // Background color
     padding: '10px 0', // Vertical padding
     textAlign: 'center', // Center text horizontally
+    color: 'white' // Text color
   };
 
   const containerStyle = {
     maxWidth: '1200px', // Limit maximum width
     margin: '0 auto', // Center container
+  };
+
+  const linkStyle = {
+    color: '#ADD8E6' // Link color (light blue)
   };
 
   return (
@@ -22,7 +27,7 @@ const Footer = () => {
         <p>
           PopHits.org by Asle
           {isAuthenticated && (
-            <a href="mailto:contact@pophits.org"> - Contact</a>
+            <a href="mailto:contact@pophits.org" style={linkStyle}> - Contact</a>
           )}
         </p>
       </div>
