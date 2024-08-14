cf61835e2ffcd9c13b038cf225a146d71dc1895d
asleulv <101048079+asleulv@users.noreply.github.com>
2024-08-14 13:49:13 +0200

    Fixed responsive view for songlist

--- frontend/src/components/SongList/SongList.js
+++ frontend/src/components/SongList/SongList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { Table, Button, Space, Switch, Select } from "antd";
+import { Table, Button, Switch, Select } from "antd";
 import { getSongs } from "../../services/api";
 
 const { Option } = Select;
@@ -32,7 +32,7 @@ const SongList = () => {
 
     const pathname = location.pathname;
     const artistSlug = pathname.includes("/artist/")
-      ? pathname.split("/artist/")[1].split('/')[0]
+      ? pathname.split("/artist/")[1].split("/")[0]
       : null;
     const yearPath = pathname.includes("/year/")
       ? pathname.split("/year/")[1]
@@ -40,7 +40,16 @@ const SongList = () => {
 
     setYearFilter(yearPath || null);
     fetchData(query || "", artistSlug, yearPath, filter === "number-one");
-  }, [location.pathname, location.search, page, perPage, sortField, sortOrder, onlyNumberOneHits, yearFilter]);
+  }, [
+    location.pathname,
+    location.search,
+    page,
+    perPage,
+    sortField,
+    sortOrder,
+    onlyNumberOneHits,
+    yearFilter,
+  ]);
 
   const fetchData = async (query, artistSlug, year, numberOneFilter) => {
     try {
@@ -50,13 +59,40 @@ const SongList = () => {
 
       let data;
       if (artistSlug) {
-        data = await getSongs(page, perPage, 'artist', artistSlug, sortField, sortOrder, query, peakRankFilter);
+        data = await getSongs(
+          page,
+          perPage,
+          "artist",
+          artistSlug,
+          sortField,
+          sortOrder,
+          query,
+          peakRankFilter
+        );
         setArtistName(artistSlug.replace(/-/g, " "));
       } else if (year) {
-        data = await getSongs(page, perPage, 'year', year, sortField, sortOrder, query, peakRankFilter);
-        setArtistName(""); 
+        data = await getSongs(
+          page,
+          perPage,
+          "year",
+          year,
+          sortField,
+          sortOrder,
+          query,
+          peakRankFilter
+        );
+        setArtistName("");
       } else {
-        data = await getSongs(page, perPage, null, null, sortField, sortOrder, query, peakRankFilter);
+        data = await getSongs(
+          page,
+          perPage,
+          null,
+          null,
+          sortField,
+          sortOrder,
+          query,
+          peakRankFilter
+        );
         setArtistName("");
       }
 
@@ -89,7 +125,7 @@ const SongList = () => {
     setSearchQuery("");
 
     const artistSlug = location.pathname.includes("/artist/")
-      ? location.pathname.split("/artist/")[1].split('/')[0]
+      ? location.pathname.split("/artist/")[1].split("/")[0]
       : null;
 
     if (artistSlug) {
@@ -105,11 +141,21 @@ const SongList = () => {
     const queryParams = new URLSearchParams(location.search);
     const query = queryParams.get("query");
     const artistSlug = location.pathname.includes("/artist/")
-      ? location.pathname.split("/artist/")[1].split('/')[0]
+      ? location.pathname.split("/artist/")[1].split("/")[0]
       : null;
-    const path = artistSlug ? `/artist/${artistSlug}` : (yearFilter ? `/year/${yearFilter}` : '/songs');
+    const path = artistSlug
+      ? `/artist/${artistSlug}`
+      : yearFilter
+      ? `/year/${yearFilter}`
+      : "/songs";
 
-    navigate(`${path}${query ? `?query=${query}&filter=${checked ? 'number-one' : ''}` : `?filter=${checked ? 'number-one' : ''}`}`);
+    navigate(
+      `${path}${
+        query
+          ? `?query=${query}&filter=${checked ? "number-one" : ""}`
+          : `?filter=${checked ? "number-one" : ""}`
+      }`
+    );
   };
 
   const handleYearChange = (value) => {
@@ -117,11 +163,16 @@ const SongList = () => {
 
     const queryParams = new URLSearchParams(location.search);
     const query = queryParams.get("query");
-    const filter = onlyNumberOneHits ? '&filter=number-one' : '';
+    const filter = onlyNumberOneHits ? "&filter=number-one" : "";
     const artistSlug = location.pathname.includes("/artist/")
-      ? location.pathname.split("/artist/")[1].split('/')[0]
+      ? location.pathname.split("/artist/")[1].split("/")[0]
       : null;
-    let path = value === "all" ? (artistSlug ? `/artist/${artistSlug}` : '/songs') : `/year/${value}`;
+    let path =
+      value === "all"
+        ? artistSlug
+          ? `/artist/${artistSlug}`
+          : "/songs"
+        : `/year/${value}`;
 
     navigate(`${path}${query ? `?query=${query}${filter}` : filter}`);
   };
@@ -129,9 +180,9 @@ const SongList = () => {
   const capitalizeWords = (str) => {
     return str
       .toLowerCase()
-      .split(' ')
-      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(' ');
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
   };
 
   const capitalizeFirstLetter = (str) => {
@@ -144,16 +195,22 @@ const SongList = () => {
       dataIndex: "title",
       key: "title",
       sorter: true,
-      render: (text, record) => <Link to={`/songs/${record.slug}`}><strong>{text}</strong></Link>,
-      width: 250, // Adjust width as needed
+      render: (text, record) => (
+        <Link to={`/songs/${record.slug}`}>
+          <strong>{text}</strong>
+        </Link>
+      ),
+      width: 250,
     },
     {
       title: "Artist",
       dataIndex: "artist",
       key: "artist",
       sorter: true,
-      render: (text, record) => <Link to={`/artist/${record.artist_slug}`}>{text}</Link>,
-      width: 200, // Adjust width as needed
+      render: (text, record) => (
+        <Link to={`/artist/${record.artist_slug}`}>{text}</Link>
+      ),
+      width: 200,
     },
     {
       title: "Year",
@@ -161,21 +218,28 @@ const SongList = () => {
       key: "year",
       sorter: true,
       render: (text, record) => <Link to={`/year/${text}`}>{text}</Link>,
-      width: 120, // Adjust width as needed
+      width: 120,
     },
     {
       title: "Peak Rank",
       dataIndex: "peak_rank",
       key: "peak_rank",
       sorter: true,
-      width: 120, // Adjust width as needed
+      width: 120,
     },
     {
       title: "Average Score",
       dataIndex: "average_user_score",
       key: "average_user_score",
       sorter: true,
-      width: 150, // Adjust width as needed
+      width: 150,
+    },
+    {
+      title: "Weeks on Chart",
+      dataIndex: "weeks_on_chart",
+      key: "weeks_on_chart",
+      sorter: true,
+      width: 150,
     },
   ];
 
@@ -183,46 +247,77 @@ const SongList = () => {
 
   const getHeading = () => {
     if (yearFilter) {
-      return `All ${yearFilter} hits`;
+      return `📅
+ All ${yearFilter} hits`;
     }
     if (artistName) {
-      return `All hits by ${capitalizeWords(artistName)}`;
+      return `🎤 All hits by ${capitalizeWords(artistName)}`;
     }
     if (searchQuery) {
-      return `Displaying results for "${searchQuery}"`;
+      return `🔍 Displaying results for "${searchQuery}"`;
     }
-    return "All hits";
+    return "🧾 All hits";
   };
 
   return (
     <>
-      <div className="message-container">
-        {getHeading()}
-      </div>
-      <div style={{ display: "flex", justifyContent: "center", marginBottom: 16, padding: 8 }}>
-        <Space>
-          <Space>
-            <span>#1 hits filter</span>
-            <Switch onChange={handleSwitchChange} checked={onlyNumberOneHits} />
-          </Space>
-          <Space>
-            <span>Jump to year:</span>
-            <Select
-              placeholder="Select Year"
-              style={{ width: 120 }}
-              onChange={handleYearChange}
-              value={yearFilter || "all"}
+      <div className="text-center my-4 text-xl font-bold">{getHeading()}</div>
+      <div className="flex flex-col md:flex-row justify-between gap-4 mb-4 p-4 rounded-lg">
+        <div className="flex items-center border border-blue-200 p-4 bg-white w-full md:w-1/3">
+          <span className="text-lg font-semibold mr-4">#1 hits filter</span>
+          <Switch
+            onChange={handleSwitchChange}
+            checked={onlyNumberOneHits}
+            className="transform scale-125 bg-gray-300 checked:bg-blue-600"
+          />
+        </div>
+
+        <div className="flex flex-col border border-blue-200 p-4 bg-white w-full md:w-1/3">
+          <span className="mb-2 text-lg font-semibold">Jump to year:</span>
+          <Select
+            placeholder="Select Year"
+            className="w-full text-lg"
+            onChange={handleYearChange}
+            value={yearFilter || "all"}
+            dropdownRender={(menu) => (
+              <div className="text-lg md:text-xl">{menu}</div>
+            )}
+          >
+            <Option value="all">All Years</Option>
+            {years.map((year) => (
+              <Option key={year} value={year}>
+                {year}
+              </Option>
+            ))}
+          </Select>
+        </div>
+        <div className="w-full md:w-1/3 flex items-center">
+          <Button
+            onClick={handleReset}
+            className="w-full px-6 py-3 text-lg text-black border border-pink-300 rounded flex items-center justify-center hover:bg-blue-600"
+          >
+            <svg
+              className="w-5 h-5 mr-2"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <Option value="all">All Years</Option>
-              {years.map(year => (
-                <Option key={year} value={year}>{year}</Option>
-              ))}
-            </Select>
-            <Button onClick={handleReset}>Reset</Button>
-          </Space>
-        </Space>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+            Reset filters
+          </Button>
+        </div>
       </div>
-      <div style={{ overflowX: "auto" }}>
+      <div className="block md:hidden text-center mb-2 text-gray-500">
+        <span>Swipe left/right to view more columns</span>
+      </div>
+      <div className="table-container overflow-x-auto">
         <Table
           dataSource={songs}
           columns={columns}
@@ -240,7 +335,7 @@ const SongList = () => {
           onChange={handleTableChange}
         />
       </div>
-      <div style={{ marginTop: 16 }}>
+      <div className="mt-4 text-center">
         Showing {songs.length} of {totalSongs} hit songs
       </div>
     </>
