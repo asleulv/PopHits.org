fe2643d93232dce4ecdf495856631935ce0105c8
asleulv <101048079+asleulv@users.noreply.github.com>
2025-12-16 15:13:09 +0100

    ...

--- frontend2/src/app/api/songs/route.js
+++ frontend2/src/app/api/songs/route.js
@@ -4,7 +4,7 @@ const INTERNAL_KEY = process.env.INTERNAL_API_KEY;
 const DJANGO_BACKEND_URL = 
   process.env.NODE_ENV === 'development' 
     ? 'http://127.0.0.1:8000'
-    : 'https://pophits.org';
+    : 'http://127.0.0.1:8080';
 
 export async function GET(request) {
   if (!INTERNAL_KEY) {
