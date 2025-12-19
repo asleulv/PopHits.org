df339fc50d47c9ab5e9aaa80619f1ea12720d6d2
asleulv <101048079+asleulv@users.noreply.github.com>
2025-12-19 14:51:30 +0100

    Fixed blog image path

--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -4,13 +4,24 @@ from songs.serializers import SongSerializer
 
 class BlogPostListSerializer(serializers.ModelSerializer):
     """Serializer for listing blog posts with minimal information"""
+    # Use MethodField to force the correct URL
+    featured_image = serializers.SerializerMethodField()
+
     class Meta:
         model = BlogPost
         fields = ['id', 'title', 'slug', 'meta_description', 'featured_image', 'published_date']
 
+    def get_featured_image(self, obj):
+        if obj.featured_image:
+            # This forces the public domain regardless of how the API is called
+            return f"https://pophits.org{obj.featured_image.url}"
+        return None
+
 class BlogPostDetailSerializer(serializers.ModelSerializer):
     """Serializer for detailed blog post information including related songs"""
     related_songs = SongSerializer(many=True, read_only=True)
+    featured_image = serializers.SerializerMethodField()
+    content = serializers.SerializerMethodField()
     
     class Meta:
         model = BlogPost
@@ -25,3 +36,14 @@ class BlogPostDetailSerializer(serializers.ModelSerializer):
             'updated_date',
             'related_songs'
         ]
+
+    def get_featured_image(self, obj):
+        if obj.featured_image:
+            return f"https://pophits.org{obj.featured_image.url}"
+        return None
+
+    def get_content(self, obj):
+        if obj.content:
+            # This fixes the images INSIDE the blog body that point to 8080
+            return obj.content.replace('http://127.0.0.1:8080', 'https://pophits.org')
+        return obj.content
